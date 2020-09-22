b001187e78cc363fdf4c85a0dceef57b4bab543a
velderome <54608051+velderome@users.noreply.github.com>
2020-09-23 03:43:02 +1000

    beta

---
+++ src/main/java/com/uts/rapid/clean/model/dao/TestAcceptDAO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uts.rapid.clean.model.dao;
+
+import com.uts.rapid.clean.model.Address;
+import com.uts.rapid.clean.model.Customer;
+import com.uts.rapid.clean.model.Order;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bryan Guntoro
+ */
+public class TestAcceptDAO {
+    
+    
+    public static void main (String[] args){
+        AcceptServiceDAO test1 = new AcceptServiceDAO();
+        ArrayList<Order> orderList = test1.orderList();
+        for (Order orderL : orderList)
+        {
+            Address address =  test1.findAddress(orderL.getAddress_id());
+            Customer customer = test1.findCustomer(orderL.getCustomer_id());
+            
+            System.out.println(address.getFullAddress());
+        }
+    }
+}
