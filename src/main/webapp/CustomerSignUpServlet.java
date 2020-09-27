d0dbcf544f8c42fea775611b54b86238bf0e243c
MinhLe2502 <41726102+MinhLe2502@users.noreply.github.com>
2020-09-27 17:24:37 +1000

    Add files via upload

---
+++ src/main/webapp/CustomerSignUpServlet.java
@@ -0,0 +1,79 @@
+package com.uts.rapid.clean.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.uts.rapid.clean.model.dao.CustomerDAO;
+import com.uts.rapid.clean.model.dao.CleanerDAO;
+
+public class CustomerSignUpServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        CustomerDAO customerDAO = new CustomerDAO();
+        CleanerDAO cleanerDAO = new CleanerDAO();
+        
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String confirmPassword = request.getParameter("confirmPassword");
+        String phoneNumber = request.getParameter("phoneNumber");
+        
+        customerDAO.findCustomer(email, password);
+        
+        Validator validator = new Validator();
+        validator.clear(session);
+        int validationTestPassed = 0;
+        
+        if (validator.validateName(firstName))
+            validationTestPassed++;
+        else
+            session.setAttribute("firstNameError", "Invalid name");
+        
+        if (validator.validateName(lastName))
+            validationTestPassed++;
+        else
+            session.setAttribute("lastNameError", "Invalid name");
+        
+        if (validator.validateEmail(email))
+            validationTestPassed++;
+        else
+            session.setAttribute("emailError", "Invalid email address");
+        
+        if (validator.validatePassword(password))
+            validationTestPassed++;
+        else
+            session.setAttribute("passwordError", "Invalid password");
+        
+        if (password.equals(confirmPassword))
+            validationTestPassed++;
+        else
+            session.setAttribute("confirmPasswordError", "Passwords do not match");
+        
+        if (validator.validatePhoneNumber(phoneNumber))
+            validationTestPassed++;
+        else
+            session.setAttribute("phoneNumberError", "Invalid phone number");
+        
+        if (validationTestPassed == 6) {
+            if (!customerDAO.hasCustomer(email) && !cleanerDAO.hasCleaner(email)) {
+                customerDAO.createCustomer(firstName, lastName, email, password, phoneNumber);
+                session.setAttribute("customer", customerDAO.findCustomer(email, password));
+                request.getRequestDispatcher("home.jsp").include(request, response);
+            }
+            else {
+                session.setAttribute("emailError", "Email address already in use");
+                request.getRequestDispatcher("customersignup.jsp").include(request, response);
+            }
+        }
+        else {
+            request.getRequestDispatcher("customersignup.jsp").include(request, response);
+        }
+    }
+}
--- src/main/webapp/myaccount.jsp
+++ src/main/webapp/myaccount.jsp
@@ -20,17 +20,22 @@
     </head>
     <body>
         <%
-           Customer customer = new Customer ("5f69e1da84ebe990acfef9d6","John","Doe","password123","johndoe@gmail.com","0123412333");
-           session.setAttribute("customer", customer);
-
-        
+            //Customer customer = new Customer("123","ad","ad","ad","ad","ad");
+            Customer customer = (Customer) session.getAttribute("customer");
+            Address address = (Address) session.getAttribute("address");
+            /*
+           String email = request.getParameter("emailErr");
+           String password = request.getParameter("passErr");
+           CustomerDAO customerDAO = new CustomerDAO();
+           customerDAO.findCustomer(email, password);
+           Customer customer = (Customer) session.getAttribute("customerDAO");*/
         %>
         <div id="nav-placeholder"></div>
         <header>
 
   <!-- Identity details -->
   <section class="profileHeader">
-    <h1>Hello <%customer.getFirstName();%></h1>
+    <h1>Hello ${customer.firstName}</h1>
   </section>
   <!-- Links to Social network accounts -->
 
@@ -43,12 +48,34 @@
     <hr class="sectionTitleRule">
     <hr class="sectionTitleRule2">
     <div class="section1Content">
-      <p><span>First Name :<%//customer.getFirstName();%></span></p>
-      <p><span>Last Name  :<%//customer.getLastName();%></span></p>
-      <p><span>Email      :<%//customer.getEmail();%></span></p>
-      <p><span>Phone      :<%//customer.getPhoneNumber();%></span></p>
+      <p><span>First Name :${customer.firstName}</span></p>
+      <p><span>Last Name  :${customer.lastName}</span></p>
+      <p><span>Email      :${customer.email}</span></p>
+      <p><span>Phone      :${customer.phoneNumber}</span></p>
     </div>
   </section>
+  <section class="section1">
+    <h2 class="sectionTitle">Address</h2>
+    <hr class="sectionTitleRule">
+    <hr class="sectionTitleRule2">
+    <div class="section1Content">
+      <p><span>Street Address : ${address.streetAddress}</span></p>
+      <p><span>Suburb : ${address.surburb}</span></p>
+      <p><span>State : ${address.state}</span></p>
+      <p><span>Postcode : ${address.postcode}</span></p>
+    </div>
+  </section>
+  <!-- Previous experience details -->
+  <section class="section1">
+    <h2 class="sectionTitle">Billing Details</h2>
+    <hr class="sectionTitleRule">
+    <hr class="sectionTitleRule2">
+    <div class="section1Content">
+      <p><span>Card holder name :</span></p>
+      <p><span>Card Number :</span></p>
+      <p><span>Expiry Date :</span></p>
+    </div>
+    </section>
   <!-- Previous experience details -->
   </section>
 <a href="editcustomer.jsp"><button class="button"> Edit </button></a>
