52f42c27c9f0d406cf1195127301c63f35ba26a3
Adrian Wong <55360387+adrianjw@users.noreply.github.com>
2020-10-04 06:44:44 +1100

    Update ConnectionServlet

--- src/main/java/com/uts/rapid/clean/controller/ConnectionServlet.java
+++ src/main/java/com/uts/rapid/clean/controller/ConnectionServlet.java
@@ -11,14 +11,41 @@ import com.mongodb.client.MongoDatabase;
 import com.uts.rapid.clean.model.dao.*;
 
 public class ConnectionServlet extends HttpServlet {
-
+    
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private CustomerDAO customerDAO;
+    private CleanerDAO cleanerDAO;
+    private AddressDAO addressDAO;
+    private OrderDAO orderDAO;
+    private RatingDAO ratingDAO;
+    private AcceptServiceDAO acceptServiceDAO;
+    
+    @Override
+    public void init() {
+        MongoDB mongoDB = new MongoDB();
+        mongoClient = mongoDB.getMongoClient();
+        database = mongoDB.getDatabase();
+        customerDAO = new CustomerDAO(database);
+        cleanerDAO = new CleanerDAO(database);
+        addressDAO = new AddressDAO(database);
+        orderDAO = new OrderDAO(database);
+        ratingDAO = new RatingDAO(database);
+        acceptServiceDAO = new AcceptServiceDAO(database);
+    }
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {      
-        MongoDB mongoDB = new MongoDB();
-        MongoClient mongoClient = mongoDB.getMongoClient();
-        MongoDatabase database = mongoDB.getDatabase();
         HttpSession session = request.getSession();
+        
+        session.setAttribute("mongoClient", mongoClient);
+        session.setAttribute("customerDAO", customerDAO);
+        session.setAttribute("cleanerDAO", cleanerDAO);
+        session.setAttribute("addressDAO", addressDAO);
+        session.setAttribute("orderDAO", orderDAO);
+        session.setAttribute("ratingDAO", ratingDAO);
+        session.setAttribute("acceptServiceDAO", acceptServiceDAO);
         
         session.setAttribute("firstNameError", "");
         session.setAttribute("lastNameError", "");
@@ -30,13 +57,10 @@ public class ConnectionServlet extends HttpServlet {
         session.setAttribute("bankAccountNumberError", "");
         session.setAttribute("bankAccountHolderNameError", "");
         session.setAttribute("loginError", "");
-        
-        session.setAttribute("mongoClient", mongoClient);
-        session.setAttribute("customerDAO", new CustomerDAO(database));
-        session.setAttribute("cleanerDAO", new CleanerDAO(database));
-        session.setAttribute("addressDAO", new AddressDAO(database));
-        session.setAttribute("orderDAO", new OrderDAO(database));
-        session.setAttribute("ratingDAO", new RatingDAO(database));
-        session.setAttribute("acceptServiceDAO", new AcceptServiceDAO(database));
+    }
+    
+    @Override
+    public void destroy() {
+        mongoClient.close();
     }
 }
