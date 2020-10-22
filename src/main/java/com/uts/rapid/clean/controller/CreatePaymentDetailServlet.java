fa9949e95106486630b5a8aeaf5f8cc99198d969
velderome <54608051+velderome@users.noreply.github.com>
2020-10-23 04:15:55 +1100

    Add validation for expiry Date

--- src/main/java/com/uts/rapid/clean/controller/CreatePaymentDetailServlet.java
+++ src/main/java/com/uts/rapid/clean/controller/CreatePaymentDetailServlet.java
@@ -20,44 +20,60 @@ import javax.servlet.http.HttpSession;
  */
 public class CreatePaymentDetailServlet extends HttpServlet {
 
-     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         String customerId = request.getParameter("customerId");
         PaymentDetailsDAO manager = (PaymentDetailsDAO) session.getAttribute("paymentDetailsDAO");
         String invalidCardNumber = null;
-        
+        String invalidExpiryDate = null;
+
         String cardNumber = request.getParameter("cardNumber");
-        cardNumber = cardNumber.replaceAll("\\s+","");
+        cardNumber = cardNumber.replaceAll("\\s+", "");
         // Validation purposes check if there is 16 digits
         int cardNumberLength = cardNumber.length();
-        
+
         String cvcs = request.getParameter("cvc");
         int cvc = Integer.parseInt(cvcs);
-        
+
         String expiryDate = request.getParameter("expiryDate");
-        expiryDate = expiryDate.replaceAll("\\s+","");
-        
+        expiryDate = expiryDate.replaceAll("\\s+", "");
+        // The month of the expirty date for validation purpose
+        String monthString = expiryDate.substring(0, 2);
+        int month = Integer.parseInt(monthString);
+
+        // The year of the expiry date for validation purposes
+        String yearString = expiryDate.substring(expiryDate.length() - 2);
+        int year = Integer.parseInt(yearString);
+
         String cardholderName = request.getParameter("cardholderName");
         String customer_id = request.getParameter("customerId");
-        
+
+        // Reset the session
         session.setAttribute("invalidCardNumber", null);
-        
+        session.setAttribute("invalidExpiryDate", null);
+
         // Checks if there is 16 digits
-        if (cardNumberLength == 16)
-        {
-        manager.createPaymentDetail(customer_id, cardNumber, expiryDate, cvc, cardholderName);
-        request.getRequestDispatcher("/PaymentDetailServlet?customerId=" + customerId).forward(request, response);
-        }
-        else
-        {
-            invalidCardNumber = "Invalid Card Number! Please try again.";
+        if (cardNumberLength != 16) {
+            // Set the string object to not null
+            invalidCardNumber = "NOT NULL";
             session.setAttribute("invalidCardNumber", invalidCardNumber);
             request.getRequestDispatcher("my-payment-details-add.jsp").forward(request, response);
-            
+
         }
-        
-        
+        // Check if month is between 1-12 and year is greater than equal to 20
+        else if (!(month > 0 && month <=12 && year >= 20)){
+            invalidExpiryDate = "NOT NULL";
+            session.setAttribute("invalidExpiryDate", invalidExpiryDate);
+            request.getRequestDispatcher("my-payment-details-add.jsp").forward(request, response);
+        }
+        else {
+
+            manager.createPaymentDetail(customer_id, cardNumber, expiryDate, cvc, cardholderName);
+            request.getRequestDispatcher("/PaymentDetailServlet?customerId=" + customerId).forward(request, response);
+
+        }
+
     }
 
     @Override
