0d819f24eac8b375f3072758569ca14e7928a0ae
Adrian Wong <55360387+adrianjw@users.noreply.github.com>
2020-10-24 23:05:45 +1100

    Update ConnectionServlet.java

--- src/main/java/com/uts/rapid/clean/controller/ConnectionServlet.java
+++ src/main/java/com/uts/rapid/clean/controller/ConnectionServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import com.uts.rapid.clean.model.dao.*;
 
