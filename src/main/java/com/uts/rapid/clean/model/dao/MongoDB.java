7ac3827024556d3baa0b9198690e1a8bb56073e5
Thai <tdtrungthai2000@gmail.com>
2020-10-24 21:56:17 +1100

    Add uri to MongoDB

--- src/main/java/com/uts/rapid/clean/model/dao/MongoDB.java
+++ src/main/java/com/uts/rapid/clean/model/dao/MongoDB.java
@@ -12,7 +12,7 @@ public class MongoDB implements Serializable {
     
     public MongoDB() {
         String uri = "mongodb+srv://asduser1:uts2020@cluster0.vldlh.mongodb.net/rapidclean?retryWrites=true&w=majority";
-        mongoClient = MongoClients.create();
+        mongoClient = MongoClients.create(uri);
         database = mongoClient.getDatabase("rapidclean");
     }
     
